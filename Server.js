bd5d214066b4de21f82b71e2cedc6e2113ceaadd
Brandon <rockeylemure@yahoo.com>
2021-12-19 17:10:15 -0600

    This will work

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const server = require('https').Server(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server, {
     cors: {
         origin:['https://chessgames.herokuapp.com'],

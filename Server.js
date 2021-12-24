cab2bd4f9ca5928239d18e5276e352f1089887a7
Brandon <rockeylemure@yahoo.com>
2021-12-23 18:05:31 -0600

    This is alot better

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin:['http://chessgames.herokuapp.com'],
+       origin:['http://chessgames.herokuapp.com'],
     }
 })
 var public = path.join(__dirname, 'public');
@@ -13,40 +13,203 @@ const port = process.env.PORT || 8080;
 
 app.use('/', express.static('public'))
 
+let Path = '/whiteteam.html';
 let numclients = 0;
 app.get('/', (req, res) =>{
     if(numclients === 0)
     {
          res.sendFile(path.join(public, '/whiteteam.html'));
     }
-    else if(numclients === 1)
+    if(numclients === 1)
     {
         res.sendFile(path.join(public, '/blackteam.html'))
     }
    console.log("Sent html file!");
 });
 
+class playerinfo{
+    playername = '';
+    socketid = '';
+    active = false
+    team = 'black';
+
+    New(n, s, t)
+    {
+        this.playername = n;
+        this.socketid = s;
+        this.active = true;
+        this.team = t;
+    }
+
+    erase()
+    {
+        this.active = false;
+    }
+}
+
+class roominfo{
+    constructor(r, pname, sockid, t)
+    {
+        this.room = r;
+        this.roomsize++;
+        if(!this.player1.active)
+        {
+            this.player1.New(pname, sockid, t);
+        }
+        else
+        {
+            this.player2.New(pname, sockid, t);
+        }
+    }
+
+    room = '';
+    roomsize = 0;
+    player1 = new playerinfo();
+    player2 = new playerinfo();
+
+    HasPlayer(sock_id)
+    {
+        if(this.player1.socketid === sock_id || this.player2.socketid === sock_id)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    AddPlayer(pname, sock_id)
+    {
+        if(roomsize === 2)
+        {
+            console.log('socket cannot connect, room is full!!!');
+            return false;
+        }
+        this.roomsize++;
+        this.player2.New(pname, sock_id, 'black');
+        return true;
+    }
+
+    RemovePlayer(sock_id)
+    {
+        if(this.player2.socketid === sock_id)
+        {
+            this.roomsize--;
+            this.player2.erase();
+            return {
+                room: this.room,
+                name: this.player2.playername
+            }
+        }
+        if(this.player1.socketid === sock_id)
+        {
+            this.roomsize--;
+            this.player1.erase();
+            return {
+                room: this.room,
+                name: this.player1.playername 
+            }
+        }
+        console.log('sock_id did not match any players in RemovePlayer!!');
+        return {
+            room: 'null',
+            name: null
+        }
+    }
+}
+
+let roomholder = [];
+let room = '';
+
+function FindRoom(sock_id){
+    for(let i = 0; i < roomholder.length; ++i){
+        if(roomholder[i].HasPlayer(sock_id))
+            return roomholder[i].room;
+    };
+    console.log("Error " + sock_id + " was not found in function FindRoom!");
+    return 'null';
+}
+
+function FindRoomandName(sock_id){
+    for(let i = 0; i < roomholder.length; ++i){
+        if(roomholder[i].HasPlayer(sock_id))
+        {
+            return {
+                room: roomholder[i].room,
+                p_name: roomholder[i].player1.playername
+            }
+        }
+    };
+}
+
+function DeletePlayer(sock_id)
+{
+    for(let i = 0; i < roomholder.length; ++i){
+        if(roomholder[i].HasPlayer(sock_id))
+        {
+            let obj = roomholder[i].RemovePlayer(sock_id);
+            return obj;
+        }
+    };
+
+    return {
+        room: 'null',
+        p_name: 'null'
+    }
+}
+
 io.on('connection', (socket) =>{
     if(numclients == 2)
     {
-        return;
+        numclients == -1;
     }
     console.log(socket.id);
-    socket.on('join', (room) =>{
-        numclients++;
-        socket.join(room);
-        socket.in('board').emit('enterroom', (socket.id + "has entered room!"));
+    socket.on('join', (player) =>{
+        if(roomholder.length === 0)
+        {
+            roomholder.push(new roominfo(player.room, player.PlayerName, socket.id, 'black'));
+        }
+        else
+        {
+            for(let index = 0; index < roomholder.length; ++index)
+            {
+                if(roomholder[index].room === player.room)
+                {
+                    if(!roomholder[index].AddPlayer(player.PlayerName, socket.id, 'white'))
+                    {
+                        console.log("socket cannot connect, room is full!!!");
+                        return;
+                    }
+                }
+            }
+        }
+
+        ++numclients;
+        room = player.room;
+        socket.join(player.room);
+        socket.in(player.room).emit('enterroom', (player.PlayerName + ` has entered the room: ${player.room}!`));
     })
 
-    socket.on('movemade', (thing) =>{
-        socket.in('board').emit('movesent', thing);
+    socket.on('take-piece', (thing) =>{
+        let answer = FindRoomandName(socket.id);
+        socket.in(answer.room).emit('remove-piece', thing);
         console.log(thing);
+    });
+
+    socket.on('drag-in', (obj) =>{
+        let room = FindRoom(socket.id);
+        socket.in(room).emit('monitor-drag-in', (obj));
+    });
+
+    
+    socket.on('drag-leave', (obj) => {
+        let room = FindRoom(socket.id);
+        socket.in(room).emit('monitor-drag-leave', (obj));
     });
 
     socket.on('disconnect', () =>{
         numclients--;
-        let message = socket.id + "left room!";
-        socket.in('board').emit('leaveroom', message);
+        let answer = DeletePlayer(socket.id);
+        socket.in(answer.room).emit('leaveroom', answer.name + " left the room!");
     })
 })
 
--- public/Index.js
+++ public/Index.js
@@ -9,16 +9,38 @@ let itemclicked = document.getElementById("lq");
 let Square = document.getElementById("11");
 let dragsquare = document.getElementById("11");
 let home = document.getElementById("11");
+let chesspiecehome = home;
+let lastspot = home;
 let focusenter = Square;
 let focusleave = Square;
 let chesspiece = null;
 let item = 1;
+let draged_piece = document.getElementById("lq");
+let blackteam = [];
+let whiteteam = [];
+let chesspieces = [];
 
+class playerinfo
+{
+    constructor(playername, room)
+    {
+        this.PlayerName = playername;
+        this.room = room
+    }
 
-const room = 'board';
-socket.on('connect', (first) =>{
-    socket.emit('join', (room));
-    alert("You connected to room board");
+    PlayerName = '';
+    room = '';
+}
+
+let Player = new playerinfo(prompt("Please enter your name.", ''), prompt("Please enter the room you would like to join.", ''));
+socket.on('connect', () =>{
+    socket.emit('join', (Player));
+    if(socket.connected)
+    alert("You connected to room " + Player.room);
+});
+
+socket.on('error', (message) => {
+    alert(Player.PlayerName + "has a problem with connecting! " + message);
 });
 
 socket.on('leaveroom', (message) => {
@@ -26,7 +48,7 @@ socket.on('leaveroom', (message) => {
 });
 
 socket.on('enterroom', (message) => {
-    alert("Player " + message);
+    alert(message);
 });
 /*
 class Move{
@@ -64,43 +86,122 @@ socket.on('passmove', (data) =>{
     {
         let elem = document.getElementById(item.toString() + item2.toString());
         let child = elem.firstElementChild;
-        elem.addEventListener("dragstart", function(e)
+        if(child !== null)
         {
-            home = this;
-        });
+            child.addEventListener('dragstart', (e) => {
+                chesspiecehome = child.parentElement;
+                chesspiece = child;
+                console.log('chesspiecehome.id is: ' + chesspiecehome.id);
+                console.log('child.id is: ' + child.id);
+                console.log('child.children.item(0).id is: ' + child.children.item(0).id);
+            });
+        }
+        
         elem.addEventListener("dragend", function(e)
         {
-            console.log("this id is: " + this.id);
-            console.log("focusenter id is: " + focusenter.id);
+            if(focusenter === null)
+            {
+                console.log('focusenter is equal to null!!!!');
+                 return;
+            }
+            console.log('this.id is: ' + this.id);
+            console.log('chesspiecehome.id is: ' + chesspiecehome.id);
+            console.log('chesspiecehome.children.item(0).id is: ' + chesspiecehome.children.item(0).id);
+            if(focusenter.className  === 'black drag-over' || focusenter.className === 'white drag-over' || focusenter.className === 'white' || focusenter.className === 'black')
+            {
+                console.log('focusenter.id is: ' + focusenter.id);
+                console.log('enterd drag-over!!!!!!!!!!!!');
+                console.log('focusenter.children.length is: ' + focusenter.children.length);
+                if(focusenter.children.length)
+                {
+                    console.log('focusenter.children.item(0)..item(0).ariaLabel is: ' + focusenter.children.item(0).children.item(0).ariaLabel);
+                    console.log('chesspiece.ariaLabel is: ' + chesspiece.ariaLabel);
+                    if(focusenter.children.item(0).children.item(0).ariaLabel === chesspiece.children.item(0).ariaLabel)
+                    {
+                        return;
+                    }
+                    if(focusenter.children.item(0).ariaLabel === 'light')
+                        {
+                            let cp = focusenter.children.item(0);
+                            whiteteam.push(focusenter.children.item(0));
+                            focusenter.removeChild(focusenter.children.item(0));
+                            socket.emit('take-piece', (cp.children.item(0).id));
+                        }
+                     else
+                        {
+                            let cp = focusenter.children.item(0);
+                            blackteam.push(focusenter.children.item(0));
+                            focusenter.removeChild(focusenter.children.item(0));
+                            socket.emit('take-piece', (cp.children.item(0).id));
+                        }
+                }
+                if(!chesspiecehome.children.item(0))
+                {
+                    console.log("chesspiecehome has no children to remove!");
+                    console.log("chesspiecehome.id is: " + chesspiecehome.id);
+                    console.log('chesspiece.id is: ' + chesspiece.id);
+                    return;
+                }
+                chesspiecehome.removeChild(chesspiece);
+                focusenter.appendChild(chesspiece);
+            }
+        
+            /*
             if(this.children.length)
             {
                 if(focusenter.children.length)
                 {
                     if(focusenter.children.item(0).ariaLabel == this.children.item(0).ariaLabel)
-                    return;
+                    {
+                        return;
+                    }
                     else{
+                        /*
+                        console.log('if(focusenter.children.item(0).ariaLabel == this.children.item(0).ariaLabel)');
+                        console.log('focusenter.children.item(0).id is: ' + focusenter.children.item(0).id);
+                        console.log('this.children.item(0).id is: ' + this.children.item(0));
                         focusenter.removeChild(focusenter.children.item(0));
                         focusenter.appendChild(this.children.item(0));
                         return;
+                        
                     }
                 }
-                if(focusenter.className == 'black drag-over' || focusenter.className == 'white drag-over')
+                if(focusenter.className === 'black drag-over' || focusenter.className === 'white drag-over')
                 {
+                    
+                    console.log('if(focusenter.className == black drag-over || focusenter.className == white drag-over)');
                     focusenter.appendChild(this.children.item(0));
-                    const data = focusenter.children.item(0).children.item(0).id;
+                    let cp = focusenter.children.item(0).children.item(0).id;
+                    console.log('piece: ' + cp);
+                    console.log('from: ' + this.id);
+                    console.log('to: ' + focusenter.id);
                     socket.emit('movemade', {
-                        piece: focusenter.children.item(0).children.item(0).id,
+                        piece: cp,
                         from: this.id,
                         to: focusenter.id
                     });
                     return;
+                    
                 }
                 else{
                     console.log("this.children.item(0).arialabel is: " + this.children.item(0).ariaLabel)
                     if(focusenter.parentElement.ariaLabel === this.children.item(0).children.item(0).ariaLabel)
                     return;
                     else{
+                        
+                        console.log('if(focusenter.parentElement.ariaLabel === this.children.item(0).children.item(0).ariaLabel)');
                         let pelement = focusenter.parentElement.parentElement.parentElement
+                        if(focusenter.parentElement.ariaLabel === 'dark')
+                        {
+                            whiteteam.push(pelement.children.item(0));
+                        }
+                        else
+                        {
+                            darkteam.push(pelement.children.item(0));
+                        }
+                        console.log('pelement.id is: ' + pelement.id);
+                        console.log('pelement.children.item(0).id is: ' + pelement.children.item(0).id);
+                        console.log('this.children.item(0).id is: ' + this.children.item(0).id)
                         pelement.removeChild(pelement.children.item(0));
                         pelement.appendChild(this.children.item(0));
                         socket.emit('movemade', {
@@ -108,89 +209,128 @@ socket.on('passmove', (data) =>{
                             from: this.id,
                             to: pelement.id
                         });
+                        
                     }
                 }
-            }
+            }*/
         });
         
         elem.addEventListener("mouseleave", SetSquareLeave, true);
-        elem.addEventListener("drop", function(e)
-        {
-        
-            
-        })
+
         elem.addEventListener("dragenter", function(e)
         {
             e.preventDefault();
-            focusenter = e.target;
-            this.classList.add('drag-over');
-            console.log("Drag enter " + focusenter.id);
+            console.log('this.className is: ' + this.className);
+            if(this.className === 'black' || this.className === 'white' || this.className === 'white drag-over' || this.className === 'black drag-over')
+            {
+                console.log("enterd className if statement!!!");
+                focusenter = this;
+                focusenter.classList.add('drag-over');
+            }
+            else{
+                let htl = this.parentElement;
+                while(htl.className !== 'white' || htl.className !== 'black')
+                {
+                    htl = htl.parentElement;
+                }
+                htl.classList.add('drag-over');
+            }
+            console.log('focusenter.id is: ' + focusenter.id);
+            socket.emit('drag-in', ({
+                piece: chesspiece.children.item(0).id,
+                square: focusenter.id
+            }));
         });
 
-        elem.addEventListener("dragover", function(e)
-        {
+        elem.addEventListener('dragover', (e) => {
             e.preventDefault();
-            focusenter = e.target;
         });
 
         elem.addEventListener("dragleave", function(e)
         {
+            socket.emit('drag-leave', (focusenter.id));
             this.classList.remove('drag-over');
+            lastspot = focusenter;
         });
-
 
         if(item2 === itemend && item != itemend)
         {
             ++item;
             item2 = 0;
-            console.log("item " + item.toString());
-            console.log("item2 " + item2.toString());
         }
         else if(item2 == itemend && item == itemend){
             break;
         }
 
         item2 = item2 + 1;
-        console.log("item2 + item2 + 1 " + item2.toString());
     }
-
-
-function Drop() 
-{
-    chesspiece = this;
-    if(focusenter.firstElementChild != null)
-    {
-        console.log("FocusEnter has child!!!!");
-        return;
-    }
-    focusenter.appendChild(chesspiece);
-    console.log("Focusenter's child has been replaced!!!!!")
-    focusenter.classList.remove('moves');
-    console.log("Drop Method has focusenter at " + focusenter.id);
-}
 
 function SetSquareLeave()
 {
     focusleave = this;
     focusleave.classList.remove('drag-over');
-    console.log("leave " + focusleave.id);
 }
 
-socket.on('movesent', (data) =>{
+
+socket.on('remove-piece', (data) =>{
     console.log("Move recieved to second board!");
-    let cp = document.getElementById(data.piece);
-    console.log("cp id is: " + cp.id);
-    cp = cp.parentElement;
-    console.log("cp piece is: " + cp.children.item(0).id);
-    const from = document.getElementById(data.from);
-    console.log("from id: " + from.id);
-    const to = document.getElementById(data.to);
-    console.log("to id: " + to.id);
-    console.log("from number of children: " + from.children.length);
-    from.removeChild(cp);
-    if(to.children.length)
+    let cp = document.getElementById(data);
+    if(cp === null)
     {
-        to.removeChild(to.children.item(0));
+        console.log('cp is null in Socket (take-piece)');
+        return;
     }
-    to.appendChild(cp);
+
+    let cpparent = cp.parentElement;
+    if(cpparent === null)
+    {
+        console.log('cpparent element is null in socket(take-piece)');
+        return;
+    }
+    
+    let parent = cpparent.parentElement;
+    if(parent === null)
+    {
+        console.log('parent element is null in socket(take-piece)');
+        return;
+    }
+
+    if(cp.ariaLabel === 'light')
+    {
+        whiteteam.push(cpparent);
+    }
+
+    else 
+    {
+        blackteam.push(cpparent);
+    }
+    parent.removeChild(cpparent);
 });
+
+socket.on('monitor-drag-in', (obj) => {
+    console.log("obj.square value: " + obj.square);
+    let boardspot = document.getElementById(obj.square);
+    if(boardspot === null)
+        return;
+    let cp = document.getElementById(obj.piece);
+    cp = cp.parentElement;
+    let parent = cp.parentElement;
+    parent.removeChild(cp);
+    let error = boardspot.appendChild(cp);
+    console.log("socket.on(monitor-drag-in) " + error);
+});
+
+/*
+socket.on('monitor-drag-leave', (id) => {
+    let boardsquare = document.getElementById(id);
+    if(boardsquare !== null)
+    {
+        boardsquare.classList.remove('drag-over');
+    }
+    else{
+        console.log(id + " id did not match any thing in socket.on('monitor-drag-leave')!");
+    }
+});
+*/
+
+
